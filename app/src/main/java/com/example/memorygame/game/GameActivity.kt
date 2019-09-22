a71e10b16ec45f3d0a36a079a85138fb4efe8cac
zaclimon <isaacpateau05@gmail.com>
2019-09-21 20:00:40 -0400

    Add a dialog for confirming if a user exits a game

--- app/src/main/java/com/example/memorygame/game/GameActivity.kt
+++ app/src/main/java/com/example/memorygame/game/GameActivity.kt
@@ -172,4 +172,14 @@ class GameActivity : AppCompatActivity() {
             }
         }
     }
+
+    override fun onBackPressed() {
+        // Show a dialog which might indicate if the user wants to quit a game
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(R.string.exit_dialog_title)
+        builder.setMessage(R.string.exit_dialog_text)
+        builder.setPositiveButton(R.string.ok_text) { _, _ -> finish() }
+        builder.setNegativeButton(R.string.cancel_text) { dialog, _ -> dialog.dismiss() }
+        builder.show()
+    }
 }
