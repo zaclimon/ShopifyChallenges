ae7d39827f4d5e22f66a8202de7f453480b1cbc3
zaclimon <isaacpateau05@gmail.com>
2019-10-13 15:41:35 -0400

    Streamline request verification on user registration

--- app/routes/register.py
+++ app/routes/register.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, request, jsonify
+from marshmallow import ValidationError
 from app import flask_bcrypt
 from app.schemas.userschema import UserSchema
 from app.models import db
@@ -11,27 +12,25 @@ register_bp = Blueprint("register", __name__)
 def register():
 
     if request.method == "POST":
-        # Inspired from: https://flask.palletsprojects.com/en/1.1.x/tutorial/views/#the-first-view-register
-        form_email = request.form["email"]
-        form_password = request.form["password"]
         user_schema = UserSchema()
 
-        if not form_email:
-            error = "An email is required"
-        elif not form_password:
-            error = "A password is required"
-        elif User.query.filter_by(email=form_email).first() is not None:
-            error = "The email {} is already registered!".format(form_email)
-        else:
+        try:
             # Encode the password so it can be easier to store in the DB.
             # See: https://github.com/maxcountryman/flask-bcrypt/issues/38#issuecomment-247513357
-            hashed_password = flask_bcrypt.generate_password_hash(form_password).decode('utf-8')
-            new_user = User(email=form_email, password=hashed_password)
+            user_schema.load(request.json)
+
+            json_email = request.json["email"]
+            json_password = request.json["password"]
+
+            if User.query.filter_by(email=json_email).first() is not None:
+                return jsonify(error="The email {} is already used".format(json_email)), 409
+
+            hashed_password = flask_bcrypt.generate_password_hash(json_password).decode('utf-8')
+            new_user = User(email=json_email, password=hashed_password)
             db.session.add(new_user)
             db.session.commit()
             return user_schema.dump(new_user)
-
-        error_data = {"error": error}
-        return jsonify(error_data)
+        except ValidationError as err:
+            return jsonify(error=err.messages), 400
     else:
         return "Hi! 👋"
