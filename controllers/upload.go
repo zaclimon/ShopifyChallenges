24cb8fc9100bd02a7e33a7aa08b921f9f071fa3c
zaclimon <isaacpateau05@gmail.com>
2020-05-07 11:55:36 -0400

    Implement upload to Google Cloud Storage
    - Be able to upload files from the backend
    server to Google Cloud Storage.
    - Remove GOOGLE_CLOUD_PROJECT_ID environment
    variable
    - Add CLOUD_STORAGE_IMAGES_FOLDER environment
    variable
    - Add UPLOAD_FOLDER environment variable

--- controllers/upload.go
+++ controllers/upload.go
@@ -7,6 +7,8 @@ import (
 	"fmt"
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"io"
 	"net/http"
 	"os"
 )
@@ -16,8 +18,8 @@ type UploadRequest struct {
 }
 
 func Upload(c *gin.Context) {
-	var jsonRequest UploadRequest
-	err := c.ShouldBindJSON(&jsonRequest)
+	var multipartFormRequest UploadRequest
+	err := c.ShouldBindWith(&multipartFormRequest, binding.FormMultipart)
 
 	if err != nil {
 		showResponseError(c, http.StatusBadRequest, err)
@@ -25,7 +27,7 @@ func Upload(c *gin.Context) {
 	}
 
 	// Validate that the token is valid before continuing
-	token, err := jwt.Parse(jsonRequest.Token, func(token *jwt.Token) (interface{}, error) {
+	token, err := jwt.Parse(multipartFormRequest.Token, func(token *jwt.Token) (interface{}, error) {
 		// Validate the algorithm used for signing the token.
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
@@ -39,7 +41,6 @@ func Upload(c *gin.Context) {
 		// Validate that the file is an image.
 		// Let's obtain the files first
 		form, err := c.MultipartForm()
-		fmt.Println(claims["id"])
 
 		if err != nil {
 			showResponseError(c, http.StatusBadRequest, err)
@@ -47,34 +48,81 @@ func Upload(c *gin.Context) {
 		}
 
 		files := form.File["images"]
-		for _, file := range files {
+		ctx := context.Background()
+		client, err := storage.NewClient(ctx)
+
+		if err != nil {
+			// The client has not been configured correctly
+			showResponseError(c, http.StatusInternalServerError, err)
+			return
+		}
+
+		bucketName := os.Getenv("CLOUD_STORAGE_BUCKET_NAME")
+		imagesFolderName := os.Getenv("CLOUD_STORAGE_IMAGES_FOLDER")
+		bucket := client.Bucket(bucketName)
+
+		for _, fileInfo := range files {
 			// Verify the extension of the file
-			if models.IsValidImageExtension(file.Filename) {
+			if models.IsValidImageExtension(fileInfo.Filename) {
 				// Upload to cloud storage
 				// 1. Ensure that the bucket (folder) for the user exists. Create it if not available
 				// 2. Verify if file metadata doesn't exist first.
 				// 3. Create the file metadata in the database.
 				// 4. Upload the file to the bucket (If filename already exists, don't upload it twice --> Filename based upload instead of ID based)
 				userID := claims["id"].(string)
-				ctx := context.Background()
-				client, err := storage.NewClient(ctx)
+
+				userFolder := bucket.Object(fmt.Sprintf("%s/%s/", imagesFolderName, userID))
+				_, err = userFolder.Attrs(ctx)
+
 				if err != nil {
-					// The client has not been configured correctly
+					// User folder does not exist yet
+					fmt.Println("Creating folder for user")
+					folderWriter := userFolder.NewWriter(ctx)
+					_, err := folderWriter.Write(make([]byte, 0))
+					folderWriter.Close()
+					if err != nil {
+						// Error happened while trying to create the user folder.
+						showResponseError(c, http.StatusInternalServerError, err)
+						return
+					}
+				}
+				// Save locally the uploaded file
+				fmt.Println("Saving file locally")
+				uploadFolder := os.Getenv("UPLOAD_FOLDER")
+				destinationPath := fmt.Sprintf("%s/%s", uploadFolder, fileInfo.Filename)
+				if err = c.SaveUploadedFile(fileInfo, destinationPath); err != nil {
 					showResponseError(c, http.StatusInternalServerError, err)
 					return
 				}
-
-				userBucket := client.Bucket(fmt.Sprintf("images/%s", userID))
-				_, err = userBucket.Attrs(ctx)
-
+				// Upload image to GCP
+				fmt.Println("Uploading to GCP")
+				imageObject := bucket.Object(fmt.Sprintf("%s/%s/%s", imagesFolderName, userID, fileInfo.Filename))
+				storageWriter := imageObject.NewWriter(ctx)
+				savedFileReader, err := os.Open(destinationPath)
 				if err != nil {
-					// Bucket does not exist yet
-					projectID := os.Getenv("GOOGLE_CLOUD_PROJECT_ID")
-					if err = userBucket.Create(ctx, projectID, nil); err != nil {
-						// Error happened while trying to create the user bucket.
-						showResponseError(c, http.StatusInternalServerError, err)
-					}
+					showResponseError(c, http.StatusInternalServerError, err)
+					return
 				}
+				_, err = io.Copy(storageWriter, savedFileReader)
+				if err != nil {
+					showResponseError(c, http.StatusInternalServerError, err)
+					return
+				}
+				savedFileReader.Close()
+				storageWriter.Close()
+				// We don't want to fill up the storage in the VM/container needlessly so delete the temporary image.
+				fmt.Println("Deleting temporary file on system")
+				err = os.Remove(destinationPath)
+				if err != nil {
+					showResponseError(c, http.StatusInternalServerError, err)
+					return
+				}
+				// Create database metadata
+				// 1. Compute PHash
+				// 2. Create ImageData struct
+				// 3. Create Image Struct
+				// 4. Link Image to user
+				// 5. Update user
 			}
 		}
 
