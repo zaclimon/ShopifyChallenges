3769f53b5ed9f147e37752d6c93b742ecaaf3b4f
zaclimon <isaacpateau05@gmail.com>
2020-05-21 17:03:42 -0400

    Router: Add the V1 API group
    In order to build for future API implementations, create the v1
    API group. In that case, the endpoint will be as shown below:
    
    /api/v1/"endpoint"

--- controllers/router.go
+++ controllers/router.go
@@ -16,9 +16,16 @@ func UtsuruRouter(db *models.DB) *gin.Engine {
 	env := &Env{db}
 
 	router := gin.Default()
-	router.POST("/register", env.Register)
-	router.POST("/login", env.Login)
-	router.POST("/upload", env.Upload)
-	router.POST("/search", env.Search)
+	api := router.Group("api")
+	{
+		v1 := api.Group("v1")
+		{
+			v1.POST("/register", env.Register)
+			v1.POST("/login", env.Login)
+			v1.POST("/upload", env.Upload)
+			v1.POST("/search", env.Search)
+		}
+	}
+
 	return router
 }
