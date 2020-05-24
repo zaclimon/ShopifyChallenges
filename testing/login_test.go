daf74141263364f63d0e7203568bb106714093aa
zaclimon <isaacpateau05@gmail.com>
2020-05-24 15:45:54 -0400

    Finalize tests for the login endpoint

--- testing/login_test.go
+++ testing/login_test.go
@@ -4,18 +4,19 @@ import (
 	"UtsuruConcept/controllers"
 	"bytes"
 	"encoding/json"
+	"errors"
+	"fmt"
 	"github.com/google/uuid"
 	"golang.org/x/crypto/bcrypt"
 	"net/http"
 	"net/http/httptest"
 	"os"
+	"strings"
 	"testing"
 	"time"
 )
 
 func TestLoginEndpoint(t *testing.T) {
-	dbType, mock, rows := initMocks()
-	router := controllers.UtsuruRouter(dbType)
 	endpoint := "/api/v1/login"
 
 	if err := os.Setenv("JWT_SECRET", "test"); err != nil {
@@ -23,7 +24,10 @@ func TestLoginEndpoint(t *testing.T) {
 	}
 
 	t.Run("Valid credentials", func(t *testing.T) {
+		dbType, mock, rows := initMocks()
+		router := controllers.UtsuruRouter(dbType)
 		recorder := httptest.NewRecorder()
+
 		userID, _ := uuid.Parse("47ff6ccc-5bed-4e9a-8b0f-3ea995c7ac3a")
 		userEmail := "test@test.com"
 		userPassword := "123456"
@@ -40,9 +44,101 @@ func TestLoginEndpoint(t *testing.T) {
 		requestJson, _ := json.Marshal(loginRequest)
 		request := httptest.NewRequest(http.MethodPost, endpoint, bytes.NewBuffer(requestJson))
 		router.ServeHTTP(recorder, request)
+	})
 
-		if recorder.Code != http.StatusOK {
-			t.Error("Could not register user while using valid credentials")
+	t.Run("Invalid email input", func(t *testing.T) {
+		dbType, mock, _ := initMocks()
+		router := controllers.UtsuruRouter(dbType)
+		recorder := httptest.NewRecorder()
+
+		userEmail := "testblah"
+		userPassword := "123456"
+
+		mock.ExpectQuery("SELECT").WithArgs(userEmail).WillReturnError(errors.New("Email doesn't exist"))
+
+		loginRequest := controllers.LoginRequest{
+			Email:    userEmail,
+			Password: userPassword,
+		}
+
+		requestJson, _ := json.Marshal(loginRequest)
+		request := httptest.NewRequest(http.MethodPost, endpoint, bytes.NewBuffer(requestJson))
+		router.ServeHTTP(recorder, request)
+
+		if recorder.Code != http.StatusBadRequest {
+			t.Error("User have been logged in with an invalid email input")
 		}
 	})
+
+	t.Run("Unregistered email", func(t *testing.T) {
+		dbType, mock, rows := initMocks()
+		router := controllers.UtsuruRouter(dbType)
+		recorder := httptest.NewRecorder()
+
+		userEmail := "test2@test.com"
+		userPassword := "123456"
+
+		mock.ExpectQuery("SELECT").WithArgs(userEmail).WillReturnRows(rows)
+
+		loginRequest := controllers.LoginRequest{
+			Email:    userEmail,
+			Password: userPassword,
+		}
+
+		requestJson, _ := json.Marshal(loginRequest)
+		request := httptest.NewRequest(http.MethodPost, endpoint, bytes.NewBuffer(requestJson))
+		router.ServeHTTP(recorder, request)
+
+		if recorder.Code != http.StatusNotFound {
+			t.Error("User with a unregistered email have been logged in")
+		}
+	})
+
+	t.Run("Invalid password", func(t *testing.T) {
+		dbType, mock, rows := initMocks()
+		router := controllers.UtsuruRouter(dbType)
+		recorder := httptest.NewRecorder()
+
+		userID, _ := uuid.Parse("47ff6ccc-5bed-4e9a-8b0f-3ea995c7ac3b")
+		userEmail := "test3@test.com"
+		userPassword := "123456"
+		userInvalidPassword := "1234567"
+		encryptedUserPassword, _ := bcrypt.GenerateFromPassword([]byte(userPassword), bcrypt.DefaultCost)
+
+		rows = mock.NewRows([]string{"id", "email", "password", "created_at"})
+		rows.AddRow(userID, userEmail, encryptedUserPassword, time.Now())
+		mock.ExpectQuery("SELECT").WithArgs(userEmail).WillReturnRows(rows)
+
+		loginRequest := controllers.LoginRequest{
+			Email:    userEmail,
+			Password: userInvalidPassword,
+		}
+
+		requestJson, _ := json.Marshal(loginRequest)
+		request := httptest.NewRequest(http.MethodPost, endpoint, bytes.NewBuffer(requestJson))
+		router.ServeHTTP(recorder, request)
+
+		if recorder.Code != http.StatusForbidden {
+			t.Error("Could login user with invalid password")
+		}
+	})
+
+	t.Run("Fields that are not 'username' or 'password", func(t *testing.T) {
+		dbType, _, _ := initMocks()
+		router := controllers.UtsuruRouter(dbType)
+		recorder := httptest.NewRecorder()
+
+		userEmail := "test@test.com"
+		userPassword := "123456"
+
+		randomJson := fmt.Sprintf("{bar: %s, foo: %s}", userEmail, userPassword)
+
+		request := httptest.NewRequest(http.MethodPost, endpoint, strings.NewReader(randomJson))
+		router.ServeHTTP(recorder, request)
+
+		if recorder.Code != http.StatusBadRequest {
+			t.Error("Could execute the request with random fields instead of 'username' and 'password'")
+		}
+	})
+
 }
